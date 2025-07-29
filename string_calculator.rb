70b4f2b39c74f8b84892ea4c3baa5b8f65aaea47
anshul09-test <anshulksharma09@gmail.com>
2025-07-29 16:31:10 +0530

    dev for custom delimiter test case

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,14 @@
 class StringCalculator
   def add(numbers_string)
     return 0 if numbers_string.strip.empty?
+    
     delimiter = /,|\n/
+    if numbers_string.start_with?("//")
+      custom_delim, numbers_string = numbers_string.split("\n", 2)
+      delim_char = custom_delim[2..-1]
+      delimiter = Regexp.escape(delim_char)
+    end
+    
     numbers = numbers_string.split(/#{delimiter}/).map(&:to_i)
     numbers.sum
   end
