7a9c100bb1672a3c4e5f44ca51fdf1fa62da260d
anshul09-test <anshulksharma09@gmail.com>
2025-07-29 16:28:07 +0530

    dev for new line character test and some code refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,8 @@
 class StringCalculator
   def add(numbers_string)
     return 0 if numbers_string.strip.empty?
-    return numbers_string.to_i if numbers_string =~ /^\d+$/
-
-     numbers_string.split(',').map(&:to_i).sum
+    delimiter = /,|\n/
+    numbers = numbers_string.split(/#{delimiter}/).map(&:to_i)
+    numbers.sum
   end
 end
