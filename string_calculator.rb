2b39bfb645702353556e14d834dc797de4152ffc
anshul09-test <anshulksharma09@gmail.com>
2025-07-29 16:09:13 +0530

    dev for 2 numbers test case

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def add(numbers_string)
     return 0 if numbers_string.strip.empty?
     return numbers_string.to_i if numbers_string =~ /^\d+$/
+
+     numbers_string.split(',').map(&:to_i).sum
   end
 end
