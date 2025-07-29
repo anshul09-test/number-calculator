4dce28a1ddc080f1b2ef8e090394bd82975297af
anshul09-test <anshulksharma09@gmail.com>
2025-07-29 16:10:36 +0530

    added twoline numbers and extraline number test case

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -17,4 +17,12 @@ class StringCalculatorTest < Minitest::Test
   def test_two_numbers
     assert_equal 3, @calculator.add("1,2")
   end
+
+  def test_multiple_numbers
+    assert_equal 10, @calculator.add("1,2,3,4")
+  end
+
+  def test_newline_between_numbers
+    assert_equal 6, @calculator.add("1\n2,3")
+  end
 end
